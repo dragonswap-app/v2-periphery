ea489ada35421aa78cb408ef6a118a4e562595cc
markolazic01 <markolazic01@gmail.com>
2024-06-02 17:56:07 +0200

    Adaptations pt7

--- test/PairFlash.spec.ts
+++ test/PairFlash.spec.ts
@@ -5,10 +5,10 @@ import {
   MockTimeNonfungiblePositionManager,
   MockTimeSwapRouter,
   PairFlash,
-  IUniswapV3Pool,
+  IDragonswapV2Pool,
   TestERC20,
   TestERC20Metadata,
-  IUniswapV3Factory,
+  IDragonswapV2Factory,
   NFTDescriptor,
   Quoter,
   SwapRouter,
@@ -31,7 +31,7 @@ describe('PairFlash test', () => {
   let nft: MockTimeNonfungiblePositionManager
   let token0: TestERC20
   let token1: TestERC20
-  let factory: IUniswapV3Factory
+  let factory: IDragonswapV2Factory
   let quoter: Quoter
 
   async function createPool(tokenAddressA: string, tokenAddressB: string, fee: FeeAmount, price: BigNumber) {
