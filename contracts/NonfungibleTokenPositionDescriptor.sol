57af066cc2f80abdb7a04792c2f32a0030b2a179
markolazic01 <markolazic01@gmail.com>
2024-07-01 20:31:06 +0200

    Logic adaptations (removal of inapplicable)

--- contracts/NonfungibleTokenPositionDescriptor.sol
+++ contracts/NonfungibleTokenPositionDescriptor.sol
@@ -5,22 +5,15 @@ pragma abicoder v2;
 import '@dragonswap/v2-core/contracts/interfaces/IDragonswapV2Pool.sol';
 
 import './libraries/SafeERC20Namer.sol';
-import './libraries/ChainId.sol';
 import './interfaces/INonfungiblePositionManager.sol';
 import './interfaces/INonfungibleTokenPositionDescriptor.sol';
 import './interfaces/IERC20Metadata.sol';
 import './libraries/PoolAddress.sol';
 import './libraries/NFTDescriptor.sol';
-import './libraries/TokenRatioSortOrder.sol';
 
 /// @title Describes NFT token positions
 /// @notice Produces a string containing the data URI for a JSON metadata string
 contract NonfungibleTokenPositionDescriptor is INonfungibleTokenPositionDescriptor {
-    address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address private constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address private constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-    address private constant TBTC = 0x8dAEBADE922dF735c38C80C7eBD708Af50815fAa;
-    address private constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
 
     address public immutable WSEI;
     /// @dev A null-terminated string
@@ -62,7 +55,7 @@ contract NonfungibleTokenPositionDescriptor is INonfungibleTokenPositionDescript
                 )
             );
 
-        bool _flipRatio = flipRatio(token0, token1, ChainId.get());
+        bool _flipRatio = flipRatio(token0, token1); 
         address quoteTokenAddress = !_flipRatio ? token1 : token0;
         address baseTokenAddress = !_flipRatio ? token0 : token1;
         (, int24 tick, , , , , ) = pool.slot0();
@@ -94,30 +87,14 @@ contract NonfungibleTokenPositionDescriptor is INonfungibleTokenPositionDescript
 
     function flipRatio(
         address token0,
-        address token1,
-        uint256 chainId
+        address token1
     ) public view returns (bool) {
-        return tokenRatioPriority(token0, chainId) > tokenRatioPriority(token1, chainId);
+        return tokenRatioPriority(token0) > tokenRatioPriority(token1);
     }
 
-    function tokenRatioPriority(address token, uint256 chainId) public view returns (int256) {
+    function tokenRatioPriority(address token) public view returns (int256) {
         if (token == WSEI) {
-            return TokenRatioSortOrder.DENOMINATOR;
-        }
-        if (chainId == 1) {
-            if (token == USDC) {
-                return TokenRatioSortOrder.NUMERATOR_MOST;
-            } else if (token == USDT) {
-                return TokenRatioSortOrder.NUMERATOR_MORE;
-            } else if (token == DAI) {
-                return TokenRatioSortOrder.NUMERATOR;
-            } else if (token == TBTC) {
-                return TokenRatioSortOrder.DENOMINATOR_MORE;
-            } else if (token == WBTC) {
-                return TokenRatioSortOrder.DENOMINATOR_MOST;
-            } else {
-                return 0;
-            }
+            return -100; // DENOMINATOR constant previously present in libraries/TokenRatioSortOrder.sol
         }
         return 0;
     }
